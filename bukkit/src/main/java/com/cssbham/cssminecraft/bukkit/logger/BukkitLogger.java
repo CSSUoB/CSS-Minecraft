e390a59a16207feace4cdef925bbecbc0192588b
Leonardo Bishop <me@leonardobishop.com>
2024-08-14 13:40:34 +0100

    Add better abstraction for logger

--- bukkit/src/main/java/com/cssbham/cssminecraft/bukkit/logger/BukkitLogger.java
+++ bukkit/src/main/java/com/cssbham/cssminecraft/bukkit/logger/BukkitLogger.java
@@ -1,59 +1,29 @@
 package com.cssbham.cssminecraft.bukkit.logger;
 
-import com.cssbham.cssminecraft.common.logger.Logger;
+import com.cssbham.cssminecraft.common.logger.AbstractLogger;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class BukkitLogger implements Logger {
+public class BukkitLogger extends AbstractLogger {
 
     private final JavaPlugin plugin;
-    private LoggingLevel serverLoggingLevel;
 
     public BukkitLogger(JavaPlugin plugin) {
         this.plugin = plugin;
-        serverLoggingLevel = LoggingLevel.INFO;
     }
 
     @Override
-    public LoggingLevel getServerLoggingLevel() {
-        return serverLoggingLevel;
+    protected void logInfo(String string) {
+        plugin.getLogger().info(string);
     }
 
     @Override
-    public void setServerLoggingLevel(LoggingLevel serverLoggingLevel) {
-        this.serverLoggingLevel = serverLoggingLevel;
+    protected void logError(String string) {
+        plugin.getLogger().severe(string);
     }
 
     @Override
-    public void log(String str, LoggingLevel level) {
-        if (serverLoggingLevel.getNumericVerbosity() < level.getNumericVerbosity()) {
-            return;
-        }
-        switch (level) {
-            case DEBUG -> plugin.getLogger().info("DEBUG: " + str);
-            case INFO -> plugin.getLogger().info(str);
-            case ERROR -> plugin.getLogger().severe(str);
-            case WARNING -> plugin.getLogger().warning(str);
-        }
-    }
-
-    @Override
-    public void debug(String str) {
-        log(str, LoggingLevel.DEBUG);
-    }
-
-    @Override
-    public void info(String str) {
-        log(str, LoggingLevel.INFO);
-    }
-
-    @Override
-    public void warning(String str) {
-        log(str, LoggingLevel.WARNING);
-    }
-
-    @Override
-    public void severe(String str) {
-        log(str, LoggingLevel.ERROR);
+    protected void logWarning(String string) {
+        plugin.getLogger().warning(string);
     }
 
 }
--- forge/src/main/java/com/cssbham/cssminecraft/forge/logger/ForgeLogger.java
+++ forge/src/main/java/com/cssbham/cssminecraft/forge/logger/ForgeLogger.java
@@ -1,59 +1,29 @@
 package com.cssbham.cssminecraft.forge.logger;
 
-import com.cssbham.cssminecraft.common.logger.Logger;
+import com.cssbham.cssminecraft.common.logger.AbstractLogger;
 import org.apache.logging.log4j.LogManager;
 
-public class ForgeLogger implements Logger {
+public class ForgeLogger extends AbstractLogger {
 
     private final org.apache.logging.log4j.Logger logger;
-    private LoggingLevel serverLoggingLevel;
 
     public ForgeLogger(String name) {
         this.logger = LogManager.getLogger(name);
-        serverLoggingLevel = LoggingLevel.INFO;
     }
 
     @Override
-    public LoggingLevel getServerLoggingLevel() {
-        return serverLoggingLevel;
+    protected void logInfo(String string) {
+        logger.info(string);
     }
 
     @Override
-    public void setServerLoggingLevel(LoggingLevel serverLoggingLevel) {
-        this.serverLoggingLevel = serverLoggingLevel;
+    protected void logError(String string) {
+        logger.error(string);
     }
 
     @Override
-    public void log(String str, LoggingLevel level) {
-        if (serverLoggingLevel.getNumericVerbosity() < level.getNumericVerbosity()) {
-            return;
-        }
-        switch (level) {
-            case DEBUG -> logger.info("DEBUG: " + str);
-            case INFO -> logger.info(str);
-            case ERROR -> logger.error(str);
-            case WARNING -> logger.warn(str);
-        }
-    }
-
-    @Override
-    public void debug(String str) {
-        log(str, LoggingLevel.DEBUG);
-    }
-
-    @Override
-    public void info(String str) {
-        log(str, LoggingLevel.INFO);
-    }
-
-    @Override
-    public void warning(String str) {
-        log(str, LoggingLevel.WARNING);
-    }
-
-    @Override
-    public void severe(String str) {
-        log(str, LoggingLevel.ERROR);
+    protected void logWarning(String string) {
+        logger.warn(string);
     }
 
 }
